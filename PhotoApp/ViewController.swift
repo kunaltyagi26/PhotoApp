1f741988e2581dc551ec6671b98be82fe9c14cf0
Kunal Tyagi <kunal.tyagi@nagarro.com>
2021-02-16 12:13:06 +0530

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by kunaltyagi on 16/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
